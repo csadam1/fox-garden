7fb64812893f90869a018eee969942080fc78489
csadam1 <cseriadam77@gmail.com>
2026-06-08 20:15:35 +0200

    Add swagger for OpenApi generation

--- src/main/java/com/cherry/controller/FoxController.java
+++ src/main/java/com/cherry/controller/FoxController.java
@@ -3,6 +3,7 @@ package com.cherry.controller;
 import com.cherry.model.request.CreateFoxRequest;
 import com.cherry.model.entity.Fox;
 import com.cherry.service.FoxServiceEjbLocal;
+import io.swagger.annotations.*;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
@@ -13,6 +14,7 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/fox")
+@Api(value = "Foxes")
 @RequestScoped
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -23,12 +25,21 @@ public class FoxController {
 
     @GET
     @Path("/all")
+    @ApiOperation(value = "Get all foxes")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "User found")
+    })
     public List<Fox> getAllFoxes() {
         return foxServiceEjb.getAllFoxes();
     }
 
     @GET
     @Path("/{id}")
+    @ApiOperation(value = "Get fox by ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Fox found"),
+            @ApiResponse(code = 404, message = "Fox not found")
+    })
     public Fox getFoxById(@PathParam("id") final int id) {
         return foxServiceEjb.getFoxById(id);
     }
@@ -36,6 +47,11 @@ public class FoxController {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Create fox")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Fox created"),
+            @ApiResponse(code = 400, message = "Invalid fox data")
+    })
     public Response createFox(@Valid final CreateFoxRequest request) {
         Fox created = foxServiceEjb.createFox(request);
         return Response.status(Response.Status.CREATED)
@@ -45,6 +61,11 @@ public class FoxController {
 
     @DELETE
     @Path("/{id}")
+    @ApiOperation(value = "Delete fox by ID")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Fox deleted"),
+            @ApiResponse(code = 404, message = "Fox not found")
+    })
     public Response deleteFoxById(@PathParam("id") final int id) {
         foxServiceEjb.deleteFoxById(id);
         return Response.status(Response.Status.NO_CONTENT).build();
--- src/main/java/com/cherry/model/request/CreateFoxRequest.java
+++ src/main/java/com/cherry/model/request/CreateFoxRequest.java
@@ -1,6 +1,8 @@
 package com.cherry.model.request;
 
 import com.cherry.validation.ValidGender;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,17 +13,22 @@ import javax.validation.constraints.NotNull;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "Fox creation request")
 public class CreateFoxRequest {
 
+    @ApiModelProperty(value = "Name of the fox", example = "Foxy", required = true)
     @NotBlank(message = "Name cannot be empty")
     private String name;
 
+    @ApiModelProperty(value = "Specie of the fox", example = "Red Fox", required = true)
     @NotBlank(message = "Species cannot be empty")
     private String species;
 
+    @ApiModelProperty(value = "Gender of the fox", example = "MALE", required = true)
     @NotNull(message = "Gender cannot be empty")
     @ValidGender(message = "Gender value can be only MALE or FEMALE")
     private String gender;
 
+    @ApiModelProperty(value = "Image of the fox", example = "foxy.jpg")
     private String image;
 }
