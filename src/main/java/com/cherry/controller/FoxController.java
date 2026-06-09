7d157bd1b340fa5691c1f3e4d7a8dca214689565
csadam1 <cseriadam77@gmail.com>
2026-06-09 20:45:46 +0200

    Upgrade Swagger to OpenAPI 3.0

--- src/main/java/com/cherry/controller/FoxController.java
+++ src/main/java/com/cherry/controller/FoxController.java
@@ -3,7 +3,13 @@ package com.cherry.controller;
 import com.cherry.model.request.CreateFoxRequest;
 import com.cherry.model.entity.Fox;
 import com.cherry.service.FoxServiceEjbLocal;
-import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
@@ -14,10 +20,10 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/fox")
-@Api(value = "Foxes")
 @RequestScoped
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "Fox", description = "Operations related to fox management")
 public class FoxController {
 
     @EJB
@@ -25,9 +31,14 @@ public class FoxController {
 
     @GET
     @Path("/all")
-    @ApiOperation(value = "Get all foxes")
+    @Operation(summary = "Get all foxes", tags = {"Fox"})
     @ApiResponses({
-            @ApiResponse(code = 200, message = "User found")
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of foxes",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Fox.class))
+            )
     })
     public List<Fox> getAllFoxes() {
         return foxServiceEjb.getAllFoxes();
@@ -35,10 +46,14 @@ public class FoxController {
 
     @GET
     @Path("/{id}")
-    @ApiOperation(value = "Get fox by ID")
+    @Operation(summary = "Get fox by ID", tags = {"Fox"})
     @ApiResponses({
-            @ApiResponse(code = 200, message = "Fox found"),
-            @ApiResponse(code = 404, message = "Fox not found")
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Fox found",
+                    content = @Content(schema = @Schema(implementation = Fox.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "Fox not found")
     })
     public Fox getFoxById(@PathParam("id") final int id) {
         return foxServiceEjb.getFoxById(id);
@@ -47,12 +62,21 @@ public class FoxController {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = "Create fox")
+    @Operation(summary = "Create a new fox", tags = {"Fox"})
     @ApiResponses({
-            @ApiResponse(code = 201, message = "Fox created"),
-            @ApiResponse(code = 400, message = "Invalid fox data")
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Fox created",
+                    content = @Content(schema = @Schema(implementation = Fox.class))
+            ),
+            @ApiResponse(responseCode = "400", description = "Invalid fox data")
     })
-    public Response createFox(@Valid final CreateFoxRequest request) {
+    public Response createFox(
+            @Valid
+            @RequestBody(description = "Fox creation request", required = true,
+                    content = @Content(schema = @Schema(implementation = CreateFoxRequest.class)))
+            final CreateFoxRequest request)
+    {
         Fox created = foxServiceEjb.createFox(request);
         return Response.status(Response.Status.CREATED)
                 .entity(created)
@@ -61,10 +85,10 @@ public class FoxController {
 
     @DELETE
     @Path("/{id}")
-    @ApiOperation(value = "Delete fox by ID")
+    @Operation(summary = "Delete fox by ID", tags = {"Fox"})
     @ApiResponses({
-            @ApiResponse(code = 204, message = "Fox deleted"),
-            @ApiResponse(code = 404, message = "Fox not found")
+            @ApiResponse(responseCode = "204", description = "Fox deleted"),
+            @ApiResponse(responseCode = "404", description = "Fox not found")
     })
     public Response deleteFoxById(@PathParam("id") final int id) {
         foxServiceEjb.deleteFoxById(id);
--- src/main/java/com/cherry/model/request/CreateFoxRequest.java
+++ src/main/java/com/cherry/model/request/CreateFoxRequest.java
@@ -1,8 +1,7 @@
 package com.cherry.model.request;
 
 import com.cherry.validation.ValidGender;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,22 +12,22 @@ import javax.validation.constraints.NotNull;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ApiModel(description = "Fox creation request")
+@Schema(description = "Fox creation request")
 public class CreateFoxRequest {
 
-    @ApiModelProperty(value = "Name of the fox", example = "Foxy", required = true)
+    @Schema(description = "Name of the fox", example = "Foxy", required = true)
     @NotBlank(message = "Name cannot be empty")
     private String name;
 
-    @ApiModelProperty(value = "Specie of the fox", example = "Red Fox", required = true)
+    @Schema(description = "Species of the fox", example = "Red Fox", required = true)
     @NotBlank(message = "Species cannot be empty")
     private String species;
 
-    @ApiModelProperty(value = "Gender of the fox", example = "MALE", required = true)
+    @Schema(description = "Gender of the fox", example = "MALE", required = true)
     @NotNull(message = "Gender cannot be empty")
     @ValidGender(message = "Gender value can be only MALE or FEMALE")
     private String gender;
 
-    @ApiModelProperty(value = "Image of the fox", example = "foxy.jpg")
+    @Schema(description = "Image filename of the fox", example = "foxy.jpg")
     private String image;
 }
